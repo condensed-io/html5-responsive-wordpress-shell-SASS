7c3494bcc19ff866a3a2ffe0c0fbe95c4baa63d0
Brent Lagerman <brent@mimoymima.com>
2013-09-03 04:16:59 -0400

    updating the ipad resizing code
    better support

--- js/functions.js
+++ js/functions.js
@@ -2,17 +2,28 @@
 // last edited: Nov 27, 2011
 
 
-// fix for ipad resizing content on orientation change by Jeremy Keith
+// fix for ipad resizing content on orientation change -- updated version of jeremy keith's fix
 // useful for making responsive sites, if your site isn't responsive you can remove this bit of code
-if (navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i)) {
-var viewportmeta = document.querySelector('meta[name="viewport"]');
-if (viewportmeta) {
-    viewportmeta.content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0, initial-scale=1.0';
-    document.body.addEventListener('gesturestart', function () {
-        viewportmeta.content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
-    }, false);
+(function(doc) {
+
+	var addEvent = 'addEventListener',
+	type = 'gesturestart',
+	qsa = 'querySelectorAll',
+	scales = [1, 1],
+	meta = qsa in doc ? doc[qsa]('meta[name=viewport]') : [];
+
+	function fix() {
+		meta.content = 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1];
+		doc.removeEventListener(type, fix, true);
 	}
-}
+
+	if ((meta = meta[meta.length - 1]) && addEvent in doc) {
+		fix();
+		scales = [0.25, 1.6];
+		doc[addEvent](type, fix, true);
+	}
+
+}(document));
 
 
 // DOCUMENT READY FUNCTION: uses noConflict to work with other libraries
