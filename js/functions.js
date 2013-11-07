3c019b2f663b563fa33d847dd6fa55bc13631c64
Brent Lagerman <brent@mimoymima.com>
2013-11-07 11:11:55 -0500

    changing out space for tabs to fix a compile problem in code kit

--- js/functions.js
+++ js/functions.js
@@ -45,18 +45,17 @@ jQuery(document).ready(function($) {
 		.addClass('make-link') // make headings look like links
 		.addClass('header-hidden')
 		.click(function(){
-	        var $this = $(this);
-	        if( $this.is('.header-shown') ) {
-	                $this.next().slideToggle('normal');
-	                $this.removeClass('header-shown');
-	                $this.addClass('header-hidden');
-	        }
-	        else {
-	                $this.next().slideToggle('normal');
-	                $this.removeClass('header-hidden');
-	                $this.addClass('header-shown');
-	        }
-	        return false;
+			var $this = $(this);
+			if( $this.is('.header-shown') ) {
+				$this.next().slideToggle('normal');
+				$this.removeClass('header-shown');
+				$this.addClass('header-hidden');
+			} else {
+				$this.next().slideToggle('normal');
+				$this.removeClass('header-hidden');
+				$this.addClass('header-shown');
+			}
+		return false;
 	});
 
 
