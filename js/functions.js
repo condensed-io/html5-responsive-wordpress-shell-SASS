020bf01f8fbe1b6ab67bbf10b48b03c92a8482a5
Brent Lagerman <brent@mimoymima.com>
2013-11-10 14:02:03 -0500

    updates to the functions.js file, finally found a good scroll to script
    also commented out some of the scripts by default, so you have to turn
    them on to make them work by uncommenting.  May put some conditional
    code in later so they don't need to start out commented out (todo)

--- js/functions.js
+++ js/functions.js
@@ -54,40 +54,70 @@ jQuery(document).ready(function($) {
 
 /* ::: MOBILE NAV ::::::::::::::::::::::::::::::::: */
 
-	$(function() {
-		var mobileBtn = $('.icon-list');
-		var menu = $('.topnav-links');
-		var menuHeight  = menu.height();
-		$(mobileBtn).on('click', function(e) {
-			e.preventDefault();
-			menu.slideToggle();
-		});
-	});
-
-	// If the browser size gets bigger than 500px make the nav visible again
-	$(window).resize(function(){
-		var w = $(window).width();
-		var menu = $('.topnav-links');
-		if(w > 900 && menu.is(':hidden')) {
-			menu.css( "display", "block" );
-		}
-	});
+//uncomment these lines to use//////////	$(function() {
+//uncomment these lines to use//////////		var mobileBtn = $('.icon-list');
+//uncomment these lines to use//////////		var menu = $('.topnav-links');
+//uncomment these lines to use//////////		//unused// var menuHeight  = menu.height();
+//uncomment these lines to use//////////		$(mobileBtn).on('click', function(e) {
+//uncomment these lines to use//////////			e.preventDefault();
+//uncomment these lines to use//////////			menu.slideToggle();
+//uncomment these lines to use//////////		});
+//uncomment these lines to use//////////	});
+//uncomment these lines to use//////////
+//uncomment these lines to use//////////	// If the browser size gets bigger than 500px make the nav visible again
+//uncomment these lines to use//////////	$(window).resize(function(){
+//uncomment these lines to use//////////		var w = $(window).width();
+//uncomment these lines to use//////////		var menu = $('.topnav-links');
+//uncomment these lines to use//////////		if(w > 900 && menu.is(':hidden')) {
+//uncomment these lines to use//////////			menu.css( "display", "block" );
+//uncomment these lines to use//////////		}
+//uncomment these lines to use//////////	});
 
 
 
 /* ::: STICKY NAV ::::::::::::::::::::::::::::::::: */
 	
-	// Check the initial Position of the Sticky Header
-	var stickyNavTop = $('.menu-mainnav-container').offset().top;
+//uncomment these lines to use//////////	// Check the initial position of the sticky header
+//uncomment these lines to use//////////	var stickyNavTop = $('.sticky').offset().top;
+//uncomment these lines to use//////////
+//uncomment these lines to use//////////    $(window).bind('scroll', function() { // try this instead: $(window).scroll(function(){
+//uncomment these lines to use//////////		if ($(window).scrollTop() > stickyNavTop) {
+//uncomment these lines to use//////////			$('.sticky').addClass('fixed');
+//uncomment these lines to use//////////		}
+//uncomment these lines to use//////////		else {
+//uncomment these lines to use//////////			$('.sticky').removeClass('fixed');
+//uncomment these lines to use//////////		}
+//uncomment these lines to use//////////    });
 
-    $(window).bind('scroll', function() { // try this instead: $(window).scroll(function(){
-		if ($(window).scrollTop() > stickyNavTop) {
-			$('.menu-mainnav-container').addClass('fixed');
-		}
-		else {
-			$('.menu-mainnav-container').removeClass('fixed');
-		}
-    });
+
+
+/* ::: SCROLL TO ELEMENTS ::::::::::::::::::::::::::::::::: */
+
+// L I N K //  http://andrewhenderson.me/tutorial/jquery-sticky-sidebar/
+	
+//uncomment these lines to use//////////	// selector for the links you want to activate
+//uncomment these lines to use//////////	// the links need to have a hash that leads to an element on the page with the same ID (which is how you probably makred it up right)
+//uncomment these lines to use//////////	$('.topnav-links a').click(function(){
+//uncomment these lines to use//////////	
+//uncomment these lines to use//////////		var el = $(this).attr('href');
+//uncomment these lines to use//////////		var elWrapped = $(el);
+//uncomment these lines to use//////////		
+//uncomment these lines to use//////////		scrollToDiv(elWrapped,40);
+//uncomment these lines to use//////////		return false;
+//uncomment these lines to use//////////		
+//uncomment these lines to use//////////	});
+//uncomment these lines to use//////////		
+//uncomment these lines to use//////////	function scrollToDiv(element,navheight){
+//uncomment these lines to use//////////		
+//uncomment these lines to use//////////		var offset = element.offset();
+//uncomment these lines to use//////////		var offsetTop = offset.top;
+//uncomment these lines to use//////////		var totalScroll = offsetTop-navheight;
+//uncomment these lines to use//////////		
+//uncomment these lines to use//////////		$('body,html').animate({
+//uncomment these lines to use//////////		scrollTop: totalScroll
+//uncomment these lines to use//////////		}, 500);
+//uncomment these lines to use//////////	
+//uncomment these lines to use//////////	}
 
 
 
@@ -112,21 +142,6 @@ jQuery(document).ready(function($) {
 
 /* ::: POPUP LINKS ::::::::::::::::::::::::::::::::: */
 	$('.popup').attr('target', '_blank');
-
-
-
-/* ::: SCROLL INTO VIEW ::::::::::::::::::::::::::::::::: */
-	
-	// https://github.com/litera/jquery-scrollintoview 
-
-	(function(f){var c={vertical:{x:false,y:true},horizontal:{x:true,y:false},both:{x:true,y:true},x:{x:true,y:false},y:{x:false,y:true}};var b={duration:"fast",direction:"both"};var e=/^(?:html)$/i;var g=function(k,j){j=j||(document.defaultView&&document.defaultView.getComputedStyle?document.defaultView.getComputedStyle(k,null):k.currentStyle);var i=document.defaultView&&document.defaultView.getComputedStyle?true:false;var h={top:(parseFloat(i?j.borderTopWidth:f.css(k,"borderTopWidth"))||0),left:(parseFloat(i?j.borderLeftWidth:f.css(k,"borderLeftWidth"))||0),bottom:(parseFloat(i?j.borderBottomWidth:f.css(k,"borderBottomWidth"))||0),right:(parseFloat(i?j.borderRightWidth:f.css(k,"borderRightWidth"))||0)};return{top:h.top,left:h.left,bottom:h.bottom,right:h.right,vertical:h.top+h.bottom,horizontal:h.left+h.right}};var d=function(h){var j=f(window);var i=e.test(h[0].nodeName);return{border:i?{top:0,left:0,bottom:0,right:0}:g(h[0]),scroll:{top:(i?j:h).scrollTop(),left:(i?j:h).scrollLeft()},scrollbar:{right:i?0:h.innerWidth()-h[0].clientWidth,bottom:i?0:h.innerHeight()-h[0].clientHeight},rect:(function(){var k=h[0].getBoundingClientRect();return{top:i?0:k.top,left:i?0:k.left,bottom:i?h[0].clientHeight:k.bottom,right:i?h[0].clientWidth:k.right}})()}};f.fn.extend({scrollintoview:function(j){j=f.extend({},b,j);j.direction=c[typeof(j.direction)==="string"&&j.direction.toLowerCase()]||c.both;var n="";if(j.direction.x===true){n="horizontal"}if(j.direction.y===true){n=n?"both":"vertical"}var l=this.eq(0);var i=l.closest(":scrollable("+n+")");if(i.length>0){i=i.eq(0);var m={e:d(l),s:d(i)};var h={top:m.e.rect.top-(m.s.rect.top+m.s.border.top),bottom:m.s.rect.bottom-m.s.border.bottom-m.s.scrollbar.bottom-m.e.rect.bottom,left:m.e.rect.left-(m.s.rect.left+m.s.border.left),right:m.s.rect.right-m.s.border.right-m.s.scrollbar.right-m.e.rect.right};var k={};if(j.direction.y===true){if(h.top<0){k.scrollTop=m.s.scroll.top+h.top}else{if(h.top>0&&h.bottom<0){k.scrollTop=m.s.scroll.top+Math.min(h.top,-h.bottom)}}}if(j.direction.x===true){if(h.left<0){k.scrollLeft=m.s.scroll.left+h.left}else{if(h.left>0&&h.right<0){k.scrollLeft=m.s.scroll.left+Math.min(h.left,-h.right)}}}if(!f.isEmptyObject(k)){if(e.test(i[0].nodeName)){i=f("html,body")}i.animate(k,j.duration).eq(0).queue(function(o){f.isFunction(j.complete)&&j.complete.call(i[0]);o()})}else{f.isFunction(j.complete)&&j.complete.call(i[0])}}return this}});var a={auto:true,scroll:true,visible:false,hidden:false};f.extend(f.expr[":"],{scrollable:function(k,i,n,h){var m=c[typeof(n[3])==="string"&&n[3].toLowerCase()]||c.both;var l=(document.defaultView&&document.defaultView.getComputedStyle?document.defaultView.getComputedStyle(k,null):k.currentStyle);var o={x:a[l.overflowX.toLowerCase()]||false,y:a[l.overflowY.toLowerCase()]||false,isRoot:e.test(k.nodeName)};if(!o.x&&!o.y&&!o.isRoot){return false}var j={height:{scroll:k.scrollHeight,client:k.clientHeight},width:{scroll:k.scrollWidth,client:k.clientWidth},scrollableX:function(){return(o.x||o.isRoot)&&this.width.scroll>this.width.client},scrollableY:function(){return(o.y||o.isRoot)&&this.height.scroll>this.height.client}};return m.y&&j.scrollableY()||m.x&&j.scrollableX()}})})(jQuery);
-
-	// Example: the following element is the link that triggers the scroll
-	$("#mylink").click(function(e){
-		e.preventDefault();
-		// and this is the thing to scroll to (footer in this example)
-		$("#footer").scrollintoview({ duration: 2000 });
-	});
 
 
 
