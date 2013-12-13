7219b8cee35287039ba3ecfb70277d8744a22507
Brent Lagerman <brent@mimoymima.com>
2013-12-13 00:42:41 -0500

    Adding a function that detects if a page is a child of another page

--- functions.php
+++ functions.php
@@ -75,7 +75,26 @@ if (function_exists('add_theme_support')) {
 
 }
 
-// Add to the body_class function
+
+// CONDITIONAL FUNCTION to determine if a page is a child of another page
+// call it like this: if(tree()=="myPageSlug1"){ echo "Hello World"; }
+    function tree(){
+      $class = '';
+      if( is_page() ) { 
+      global $post;
+          /* Get an array of Ancestors and Parents if they exist */
+      $parents = get_post_ancestors( $post->ID );
+          /* Get the top Level page->ID count base 1, array base 0 so -1 */ 
+      $id = ($parents) ? $parents[count($parents)-1]: $post->ID;
+         /* Get the parent and set the $class with the page slug (post_name) */
+      $parent = get_page( $id );
+      $class = $parent->post_name;
+    }
+    return $class;
+    }
+
+
+// Improves the body_class function
 function condensed_body_class($classes) {
     global $post;
 
