35c9f7d75d7482601e7fe503be6772d6a68b26f3
Brent Lagerman <brent@mimoymima.com>
2014-05-09 01:00:50 -0400

    FINALLY Enqueueing javascript and getting scripts into the footer

--- functions.php
+++ functions.php
@@ -29,22 +29,33 @@ remove_action('wp_head', 'wp_generator');
 
 
 
+/* ::: LOAD SCRIPTS ::::::::::::::::::::::::::::::::: */
 
-    function my_init_method() {
+    function load_my_scripts_yo() {
         if (!is_admin()) {
 
             wp_deregister_script( 'jquery' );
 
             // local copy of jquery
-            wp_register_script( 'jquery', '/wp-includes/js/jquery/jquery.js"');
+            wp_register_script( 'jquery', '/wp-includes/js/jquery/jquery.js',null,null,true);
 
             // google CDN copy of jquery
-            //wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
+            // wp_register_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
             
             wp_enqueue_script( 'jquery' );
+            
+            // javascript functions
+            wp_register_script( 'functions', get_template_directory_uri().'/js/functions.js', array('jquery'),'1.1', true);
+            wp_enqueue_script( 'functions' );
+
+            // modernizr
+            wp_register_script( 'modernizr', get_template_directory_uri().'/js/modernizr.custom.js',null,null,true);
+            wp_enqueue_script( 'modernizr' );
+
         }
     } 
-    add_action('init', 'my_init_method');
+    add_action('wp_enqueue_scripts', 'load_my_scripts_yo');
+
 
 
 /* ::: ADD THEME SUPPORT ::::::::::::::::::::::::::::::::: */
