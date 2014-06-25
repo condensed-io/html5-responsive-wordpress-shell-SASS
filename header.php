f5e29efe19dfaede6d21285ff6fef5887a47a05e
Brent Lagerman <brent@mimoymima.com>
2014-06-25 17:49:33 -0400

    adding a version number for cache busting

--- header.php
+++ header.php
@@ -5,14 +5,14 @@
 <head>
 
 	<meta charset="<?php bloginfo( 'charset' ); // lets you change the charset from within wp, defaults to UTF8 ?>" />
-	
+
 	<!--Forces latest IE rendering engine & chrome frame-->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
 	<!-- title & meta handled by the yoast plugin, don't add your own here just activate the plugin -->
 
 	<title><?php wp_title(''); ?></title>
-	
+
 	<!-- favicon & other link Tags -->
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
 	<link rel="icon" href="/favicon.ico" type="image/x-icon" />
@@ -24,25 +24,25 @@
 	<!--ADD GOOGLE FONTS HERE-->
 
 	<!-- CSS -->
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" media="screen" />
+	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css?v1.00" media="screen" />
 
 	<!-- BEGIN: IE Specific Hacks -->
 	<!--[if IE 9]><link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/styles/ie9.css" media="screen" /><![endif]-->
 	<!--[if IE 8]><link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/styles/ie8.css" media="screen" /><![endif]-->
 	<!--[if IE 7]><link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/styles/ie7.css" media="screen" /><![endif]-->
 	<!--END: IE Specific Hacks-->
-	
+
 	<!--REMOVE this viewport code if you are making a site that is NOT responsive-->
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
  	<!--REMOVE this viewport code if you are making a site that is NOT responsive-->
-	
+
 	<?php wp_head(); // wp_head hook for Plugins ~ always keep this just before the /head tag ?>
 
 </head>
 
 <!--see http://www.mimoymima.com/2010/03/lab/wordpress-body-tag/-->
 <body <?php body_class(); ?>>
-		
+
 	<!--SITE HEADER ~ for SEO ~ hidden with CSS-->
 	<header class="site-header" role="banner">
 		<h1 class="site-title"><?php if(!is_home()) { wp_title(''); echo " :: "; } ?><a href="/"><?php bloginfo('name'); ?></a></h1>
