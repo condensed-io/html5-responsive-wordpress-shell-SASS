0e122a23a91e0cbcdedc696c7216de3fe9d342ff
Brent Lagerman <brent@mimoymima.com>
2015-10-13 19:26:44 -0400

    removing a function we don't use anymore

--- functions.php
+++ functions.php
@@ -11,15 +11,6 @@
 
 // ::: UTILITY FUNCTIONS :::::::::::::::::::::::::::::::::::::::::::::
 
-    // Gets the URL of the featured image to use as a background
-    function getFeaturedURL() {
-        $post_image_id = get_post_thumbnail_id($post_to_use->ID);
-        if ($post_image_id) {
-            $thumbnail = wp_get_attachment_image_src( $post_image_id, 'post-thumbnail', false);
-            if ($thumbnail) (string)$thumbnail = $thumbnail[0];
-        }
-        echo $thumbnail;
-    }
 
     // Used to create custom length excerpts
     function get_the_custom_excerpt($length){
