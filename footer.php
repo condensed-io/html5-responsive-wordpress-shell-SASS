689896663ac45b5095107b8e7558f22c3e488969
Brent Lagerman <brent@mimoymima.com>
2015-11-02 18:08:09 -0500

    removing the vcard example

--- footer.php
+++ footer.php
@@ -10,21 +10,6 @@
 				<?php wp_nav_menu(array('menu' => 'footerNav', 'menu_class' => 'nav-footer horiz-list')); // create the footerNav menu inside Appearance menus and go to town -- for more on menus see: http://templatic.com/news/wordpress-3-0-menu-management ?>
 			</nav>
 			<!--END: Footer Nav-->
-
-			<!--BEGIN: Optional Contact Info using microformats: http://microformats.org/-->
-			<!--div class="vcard">
-				<h1 class="org fn">OrgName or Full Name of person - remove one or the other class</h1>
-				<p class="adr">
-					<span class="street-address"></span><br />
-					<span class="locality">City</span>, 
-					<span class="region">State</span><br />
-					<span class="postal-code">xxxxx</span>		
-				</p>
-				<p class="tel"></p>
-				<p class="email"><a href="mailto:"></a></p>
-				<p class="fax"></p>
-			</div-->
-			<!--END: Contact Info-->
 			
 			<!--BEGIN: Credits :: Web Design Credit -->
 			<article class="credit"><h1>Web Design by <a href="http://www.mimoymima.com" title="web design brooklyn">mimoYmima.com</a></h1></article>
