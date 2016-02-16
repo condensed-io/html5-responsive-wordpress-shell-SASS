55718d14b441a1fdece8130e5a4f777f3ee63aa9
Brent Lagerman <brent@mimoymima.com>
2016-02-15 20:47:29 -0500

    updating formatting for better readability

--- functions.php
+++ functions.php
@@ -1,22 +1,20 @@
 <?php
 
 
-//// BEGIN: if you are logged into the admin area, show what template someone is using on the top of all pages
-   if (is_user_logged_in()) { add_action('wp_footer', 'show_template'); }
-   function show_template() {
-       global $template;
-       print_r($template);
-   }
-
-
 // ::: UTILITY FUNCTIONS :::::::::::::::::::::::::::::::::::::::::::::
 
+
+    // Show Template name to Admins
+    if (is_user_logged_in()) { add_action('wp_footer', 'show_template'); }
+    function show_template() {
+       global $template;
+       print_r($template);
+    }
 
     // Used to create custom length excerpts
     function get_the_custom_excerpt($length){
         return substr( get_the_excerpt(), 0, strrpos( substr( get_the_excerpt(), 0, $length), ' ' ) ).'...';
     }
-
 
     // Function used to see if you are in a post type
     function is_post_type($type){
@@ -26,30 +24,33 @@
     }
 
 
+
 // ::: TWEAK SOME WORDPRESS DEFAULTS :::::::::::::::::::::::::::::::::
 
-    // ** For Responsive images and thumbnails, removes the width and height from the markup
+    // For Responsive images and thumbnails, removes the width and height from the markup
     add_filter( 'post_thumbnail_html', 'remove_thumbnail_dimensions', 10 );
     add_filter( 'image_send_to_editor', 'remove_thumbnail_dimensions', 10 );
+
     // Removes attached image sizes as well
     add_filter( 'the_content', 'remove_thumbnail_dimensions', 10 );
     function remove_thumbnail_dimensions( $html ) {
     $html = preg_replace( '/(width|height)=\"\d*\"\s/', "", $html );
     return $html;
     }
-    // ** Removes tags generated in the WordPress Head that we don't use, you could read up and re-enable them if you think they're needed
+
+    // Removes tags generated in the WordPress Head that we don't use, you could read up and re-enable them if you think they're needed
     remove_action('wp_head', 'wlwmanifest_link');
     remove_action('wp_head', 'rsd_link');
     remove_action('wp_head', 'wp_generator');
 
-    // ** Makes it so you can upload svgs through the Wordpress Uploader - http://css-tricks.com/snippets/wordpress/allow-svg-through-wordpress-media-uploader/
+    // Makes it so you can upload svgs through the Wordpress Uploader - http://css-tricks.com/snippets/wordpress/allow-svg-through-wordpress-media-uploader/
     function cc_mime_types( $mimes ){
         $mimes['svg'] = 'image/svg+xml';
         return $mimes;
     }
     add_filter( 'upload_mimes', 'cc_mime_types' );
 
-    // ** Improves the body_class function
+    // Improves the body_class function
     function condensed_body_class($classes) {
         global $post;
 
@@ -82,12 +83,14 @@
     add_filter('body_class', 'condensed_body_class');
 
 
-    // ** Removes the automatic paragraph tags from the excerpt, we leave it on for the content and have a custom field you can use to turn it off on a page by page basis --> wpautop = false
+    // Removes the automatic paragraph tags from the excerpt, we leave it on for the content and have a custom field you can use to turn it off on a page by page basis --> wpautop = false
         remove_filter('the_excerpt', 'wpautop');
+
+
 
 /* ::: LOAD SCRIPTS ::::::::::::::::::::::::::::::::: */
 
-    // ** Detect if page is the login page
+    // Detect if page is the login page
     function is_login_page() {
         return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
     }
@@ -132,7 +135,6 @@
 
 
 
-
 /* ::: ADD THEME SUPPORT ::::::::::::::::::::::::::::::::: */
 
     if (function_exists('add_theme_support')) {
@@ -149,7 +151,7 @@
 
 /* ::: UTILITY ::::::::::::::::::::::::::::::::: */
 
-    // ** CONDITIONAL FUNCTION to determine if a page is a child of another page
+    // CONDITIONAL FUNCTION to determine if a page is a child of another page
     // call it like this: if(tree()=="myPageSlug1"){ echo "Hello World"; }
     function tree(){
       $class = '';
