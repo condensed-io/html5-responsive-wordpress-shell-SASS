98c977753cf397f530ed494310231b0e9e2e2c3d
Brent Lagerman <brent@mimoymima.com>
2016-10-09 11:00:12 -0400

    tweaks

--- header.php
+++ header.php
@@ -9,9 +9,9 @@
 	<!--Forces latest IE rendering engine & chrome frame-->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 
-	<!-- title & meta handled by the yoast plugin, don't add your own here just activate the plugin -->
-
 	<title><?php wp_title(''); ?></title>
+
+    <!--SEO meta by Yoast plugin, don't add your own-->
 
 	<!-- favicon & other link Tags -->
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
@@ -29,9 +29,8 @@
 	<!--[if IE 7]><link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/styles/ie7.css" media="screen" /><![endif]-->
 	<!--END: IE Specific Hacks-->
 
-	<!--REMOVE this viewport code if you are making a site that is NOT responsive-->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
- 	<!--REMOVE this viewport code if you are making a site that is NOT responsive-->
+    <!--makes things responsive-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
 	<?php wp_head(); // wp_head hook for Plugins ~ always keep this just before the /head tag ?>
 
@@ -39,16 +38,23 @@
 
 <body <?php body_class(); // we add to the body classes with a function in functions.php ?>>
 
+
+<div class="hide"><?php include_once('img/sprite/symbol/svg/sprite.symbol.svg'); //loads the SVG sprite ?></div>
+
 <!--[if lt IE 9]><p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p><![endif]-->
 
-	<!--SITE HEADER ~ for SEO ~ hidden with CSS-->
+	<!--BEGIN: Site Header -->
 	<header class="site-header" role="banner">
 		<h1 class="site-title"><?php if(!is_front_page() || !is_home()) { wp_title(''); echo " :: "; } ?><a href="/"><?php bloginfo('name'); ?></a></h1>
-	</header>
-
+		
 	<!--MAIN NAV: Activate the menu system by going into wpadmin / appearance / menus / and adding a menu named mainNav-->
 	<!--To make the menu vertical instead of horizontal remove the menu_class of horiz-list-->
 	<nav class="nav-main" role="navigation">
 		<h1 class="access-hide">Main Navigation</h1>
 		<?php wp_nav_menu(array('menu' => 'mainNav', 'menu_class' => 'horiz-list')); // create the mainNav menu inside Appearance menus and go to town -- for more on menus see: http://templatic.com/news/wordpress-3-0-menu-management ?>
 	</nav>
+	
+	</header>
+	<!--END: Site Header-->
+
+
