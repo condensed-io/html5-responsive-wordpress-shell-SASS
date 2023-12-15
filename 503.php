45aaace39e58ec62d5ce878ea2836622642b748a
Brent Lagerman <brent@condensed.io>
2023-12-15 00:39:26 -0500

    better wording for this comment

--- 503.php
+++ 503.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
-<!--NOTE: you can't use includes in a 503 document so put your header and footer content in here manually-->
+<!--NOTE: you can't use wordpress functions in a 503 document so put your header and footer content in here manually-->
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 
