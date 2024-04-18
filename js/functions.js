05e6b6654d99391b98bac94d08fddcb0992a3cec
Brent Lagerman <brent@condensed.io>
2024-04-18 17:43:39 -0400

    removing old js that's no longer needed

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
-// mYm Functions v 2 - brent@condensed.io
-// last edited: Nov 7, 2023
+// mYm Functions v 3 - brent@condensed.io
+// last edited: April 18, 2024
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
@@ -8,11 +8,6 @@ $(document).ready(function(){
 //  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 //  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 
-
-/* ::: Equal Height Divs ::::::::::::::::::::::::::::::::: */
-
-var highestCol = Math.max($('#element1').height(),$('#element2').height());
-$('.elements').height(highestCol);
     
 
 /* ::: SHOW AND HIDE ::::::::::::::::::::::::::::::::: */
@@ -33,38 +28,6 @@ $('.elements').height(highestCol);
 			}
 		return false;
 	});
-
-/* ::: SCROLL TO ELEMENTS ::::::::::::::::::::::::::::::::: */
-
-// L I N K //  http://andrewhenderson.me/tutorial/jquery-sticky-sidebar/
-	
-	// selector for the links you want to activate, adding class of jumplink but you can add more by comma separating them
-	// the links need to have a hash that leads to an element on the page with the same ID (which is how you probably makred it up right)
-	$('.jumplink').click(function(){
-	
-		var el = $(this).attr('href');
-		var elWrapped = $(el);
-		
-		scrollToDiv(elWrapped,40);
-		return false;
-		
-	});
-		
-	function scrollToDiv(element,navheight){
-		
-		var offset = element.offset();
-		var offsetTop = offset.top;
-		var totalScroll = offsetTop-navheight;
-		
-		$('body,html').animate({
-		scrollTop: totalScroll
-		}, 500);
-	
-	}
-
-/* ::: POPUP LINKS ::::::::::::::::::::::::::::::::: */
-	$('.popup').attr('target', '_blank');
-
 
 //  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
 //  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
